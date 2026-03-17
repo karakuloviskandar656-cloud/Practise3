a377b9fc08879977e474d81ea5e4c4ee58e739cd
karakuloviskandar656-cloud <karakuloviskandar656@gmail.com>
2026-03-17 13:16:51 +0500

    Add basic function examples in basic_functions.py
    Added basic function examples demonstrating definition, calling, and using 'pass'.

---
+++ functions/basic_functions.py
@@ -0,0 +1,24 @@
+# In Python, a function is defined using the def keyword, followed by a function name and parentheses:
+
+def my_function():
+  print("Hello from a function")
+
+# To call a function, write its name followed by parentheses:
+
+def my_function():
+  print("Hello from a function")
+
+my_function()
+
+# You can call the same function multiple times:
+def my_function():
+  print("Hello from a function")
+
+my_function()
+my_function()
+my_function()
+
+# Function definitions cannot be empty. If you need to create a function placeholder without any code, use the pass statement:
+
+def my_function():
+  pass
